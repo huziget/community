37f33a91a71f3ce58820002e75d4f0c2d7766b2c
虎子 <huzi@huzideiMac-Pro.local>
2020-01-07 07:01:56 -0800

    editlogin

---
+++ src/main/java/life/majiang/community/community/dto/AccessTokenDTO.java
@@ -0,0 +1,49 @@
+package life.majiang.community.community.dto;
+
+public class AccessTokenDTO {
+    private String client_id;
+    private String redirect_uri;
+    private String login;
+    private String scope;
+    private String state;
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getRedirect_uri() {
+        return redirect_uri;
+    }
+
+    public void setRedirect_uri(String redirect_uri) {
+        this.redirect_uri = redirect_uri;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/life/majiang/community/community/provider/GitHubProvider.java
@@ -0,0 +1,14 @@
+package life.majiang.community.community.provider;
+
+import life.majiang.community.community.dto.AccessTokenDTO;
+import org.springframework.stereotype.Component;
+
+/**
+ * created by huang on 2020/1/7
+ */
+@Component
+public class GitHubProvider {
+    public String getAccessTokenDTO(AccessTokenDTO accessTokenDTO){
+        return null;
+    }
+}
