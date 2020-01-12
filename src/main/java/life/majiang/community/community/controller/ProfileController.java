ab9851983a86337082d4d154d10421669c78b9c5
huzi <b1976439293@gmail.com>
2020-01-12 00:00:58 -0800

    create question

--- src/main/java/life/majiang/community/community/controller/ProfileController.java
+++ src/main/java/life/majiang/community/community/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package life.majiang.community.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,8 +12,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ProfileController {
 
     @GetMapping("/profile/{action}")
-    public String profile(@PathVariable(name = "action") String action){
+    public String profile(@PathVariable(name = "action") String action, Model model){
 
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "question");
+            model.addAttribute("sectionName", "我的提问");
+        }else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新问题");
+        }
         return "profile";
     }
 
